fae4293409ecd182ba8c8f00881a4b6f2197e711
toxdes <malivp3494@gmail.com>
2026-01-10 09:38:05 +0530

    nvim: neovide specific config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,6 +5,7 @@ vim.opt.tabstop = spaces
 vim.opt.softtabstop = spaces
 vim.opt.shiftwidth = spaces
 
+
 -- fix splits
 vim.opt.splitbelow = true
 vim.opt.splitright = true
@@ -24,6 +25,11 @@ vim.g.editorconfig = true
 -- set line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- neovide specific config
+if vim.g.neovide then
+    require("config.neovide")
+end
 
 -- load package manager
 require("config.lazy")
---
+++ nvim/.config/nvim/lua/config/neovide.lua
@@ -0,0 +1,16 @@
+-- neovide (https://neovide.dev) specific config
+-- will be loaded with neovide, and not neovim
+
+-- set font and line-spacing
+vim.o.guifont = "IosBerkMono Medium:h11"
+vim.opt.linespace = 2
+
+-- set padding/margin
+vim.g.neovide_padding_top = 0
+vim.g.neovide_padding_bottom = 0
+vim.g.neovide_padding_right = 0
+vim.g.neovide_padding_left = 0
+
+-- set transparency
+vim.g.neovide_opacity = 0.95
+vim.g.neovide_normal_opacity = 0.95
